3812d2387527bd433af0afeb6be9292106aa3c54
pierrickmartino <pierrick.martino@gmail.com>
2020-12-17 11:44:03 +0100

    LEGACY

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "8b34579efb6d9e8852c332df5aace92f",
-"/": "8b34579efb6d9e8852c332df5aace92f",
-"main.dart.js": "e056bd4df5f79dea003cf5192bc4accd",
+"index.html": "e0e5af0e09b954caa446c805799f5dee",
+"/": "e0e5af0e09b954caa446c805799f5dee",
+"main.dart.js": "ea948ec1dbce485a316d52e9b4dbff53",
 "manifest.json": "f2b4aabb906537cf06de485cc4978a21",
 "version.json": "f1772e8b92f95818fe0e2a5633321c44"
 };
