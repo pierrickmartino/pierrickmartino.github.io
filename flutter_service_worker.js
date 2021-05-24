cde13f59060064f0396d8e3d7ed801faeeb64b72
pierrickmartino <pierrick.martino@gmail.com>
2021-05-24 16:38:29 +0200

    LEGACY

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,14 +6,14 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "7f45115ad715b35f0a4e15d0bf2fc8c5",
+"assets/NOTICES": "bbafe3e9651674c5703896cb564cb74c",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "firebase_init.js": "0e0f56e30d109846de2b4bd4855d298d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "0a0d69fa256e87d7431d46985066ac93",
 "/": "0a0d69fa256e87d7431d46985066ac93",
-"main.dart.js": "cb57ee8a6003dd2d1ace8d2daa75da91",
+"main.dart.js": "7694aa856c33621878b0e9e91e9911a7",
 "manifest.json": "2d215d05aaa64529b7b4adb5ad96527a",
 "version.json": "0507ec11c5c92cea90b4f83e2e3b2033"
 };
